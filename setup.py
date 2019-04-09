aa7340b37882f627da43fd2624f5aa88de4be0a7
Alexey Sukharevich <aliaksei.sukharevich@gmail.com>
2019-04-09 21:00:59 +0300

    setup project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dioptre',
+    version='0.1',
+    packages=['dioptre'],
+    author='Alexey Sukharevich',
+    author_email='aliaksei.sukharevich@gmail.com',
+    description='Optical text recognition neural network and set of tools for its training and evaluation.',
+)
